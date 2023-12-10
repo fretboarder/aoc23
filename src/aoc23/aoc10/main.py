95bd6c062cc576834f3f2d83a69f7be75e0c8816
fretboarder <cini01@gmail.com>
2023-12-10 14:22:17 +0100

    feat: day10 / part1 completed

---
+++ src/aoc23/aoc10/main.py
@@ -0,0 +1,168 @@
+from collections import deque, namedtuple
+from enum import Enum
+from pathlib import Path
+from pprint import pp
+from typing import NamedTuple
+
+from aoc23.support import get_input
+
+
+class Pos(NamedTuple):
+    col: int  # x
+    row: int  # y
+
+
+Grid = list[str]
+
+
+class Direction(Enum):
+    WEST = Pos(-1, 0)
+    EAST = Pos(1, 0)
+    NORTH = Pos(0, -1)
+    SOUTH = Pos(0, 1)
+
+
+DIRS = {
+    Direction.EAST: Pos(1, 0),
+    Direction.WEST: Pos(-1, 0),
+    Direction.NORTH: Pos(0, -1),
+    Direction.SOUTH: Pos(0, 1),
+}
+
+VALID_NEIGHBORS = {
+    Direction.NORTH: [
+        ("|", "|"),
+        ("|", "F"),
+        ("|", "7"),
+        ("L", "|"),
+        ("L", "7"),
+        ("L", "F"),
+        ("J", "|"),
+        ("J", "7"),
+        ("J", "F"),
+        ("S", "|"),
+        ("S", "7"),
+        ("S", "F"),
+    ],
+    Direction.SOUTH: [
+        ("|", "|"),
+        ("|", "L"),
+        ("|", "J"),
+        ("F", "|"),
+        ("F", "L"),
+        ("F", "J"),
+        ("7", "|"),
+        ("7", "L"),
+        ("7", "J"),
+        ("S", "|"),
+        ("S", "L"),
+        ("S", "J"),
+    ],
+    Direction.WEST: [
+        ("-", "-"),
+        ("-", "F"),
+        ("-", "L"),
+        ("7", "-"),
+        ("7", "F"),
+        ("7", "L"),
+        ("J", "-"),
+        ("J", "F"),
+        ("J", "L"),
+        ("S", "-"),
+        ("S", "F"),
+        ("S", "L"),
+    ],
+    Direction.EAST: [
+        ("-", "-"),
+        ("-", "7"),
+        ("-", "J"),
+        ("F", "-"),
+        ("F", "J"),
+        ("F", "7"),
+        ("L", "-"),
+        ("L", "J"),
+        ("L", "7"),
+        ("S", "-"),
+        ("S", "J"),
+        ("S", "7"),
+    ],
+}
+
+
+def is_valid_neighbor(
+    grid: Grid, current_pos: Pos, neighbor: Direction
+) -> tuple[bool, Pos]:
+    sym = grid[current_pos.row][current_pos.col]
+    new_pos = Pos(
+        current_pos.col + DIRS[neighbor].col, current_pos.row + DIRS[neighbor].row
+    )
+    if new_pos.col < len(grid[0]) and new_pos.row < len(grid):
+        neighbor_sym = grid[new_pos.row][new_pos.col]
+        return (sym, neighbor_sym) in VALID_NEIGHBORS[neighbor], new_pos
+    return False, new_pos
+
+
+def get_initial_pos(grid: Grid) -> Pos:
+    for r, row in enumerate(grid):
+        if (c := row.find("S")) != -1:
+            return Pos(c, r)
+    msg = "grid doesn't seem to have a starting position"
+    raise ValueError(msg)
+
+
+def get_starting_directions(
+    grid: Grid, initial_pos: Pos
+) -> tuple[Direction, Direction]:
+    dirs = [d for d in Direction if is_valid_neighbor(grid, initial_pos, d)[0]]
+    if len(dirs) == 2:  # noqa: PLR2004
+        return dirs[0], dirs[1]
+    msg = f"Expected to find 2 directions, found {len(dirs)}"
+    raise ValueError(msg)
+
+
+def bfs(grid: Grid, initial_pos: Pos, initial_dir: Direction) -> list[Pos]:
+    steps = 1
+    visited = {initial_pos}
+
+    positions: list[Pos] = []
+    _, new_pos = is_valid_neighbor(grid, initial_pos, initial_dir)
+
+    frontier = deque([(new_pos, steps)])
+
+    while frontier:
+        current_pos, steps = frontier.popleft()
+        if current_pos in visited:
+            continue
+
+        visited.add(current_pos)
+        for direction in DIRS:
+            valid_neighbor, new_pos = is_valid_neighbor(grid, current_pos, direction)
+            if new_pos not in visited and valid_neighbor:
+                positions.append(new_pos)
+                frontier.append((new_pos, steps + 1))
+
+    return positions
+
+
+def solution1(grid: Grid) -> int:
+    steps = [
+        bfs(grid, get_initial_pos(grid), d)
+        for d in get_starting_directions(grid, get_initial_pos(grid))
+    ]
+
+    for i, (p1, p2) in enumerate(zip(*steps), start=2):
+        if p1 == p2:
+            break
+
+    return i
+
+
+def main() -> tuple[int, int]:
+    lines = get_input(Path(__file__).parent / "input01.txt")
+    return solution1(lines), 0
+
+
+if __name__ == "__main__":
+    sol1, sol2 = main()
+    pp(f"Solution 1: {sol1}")  # 7173
+    pp(f"Solution 2: {sol2}")
---
+++ tests/test_day10.py
@@ -0,0 +1,103 @@
+from typing import Callable
+
+import pytest
+from aoc23.aoc10 import main
+
+g1 = [
+    "..|..",
+    ".-S-.",
+    "..|..",
+]
+
+
+@pytest.fixture()
+def grid() -> Callable:
+    def get(g1: main.Grid, startsym: str) -> list[str]:
+        g = [*g1]
+        g[1] = g1[1].replace("S", startsym)
+        return g
+
+    return get
+
+
+@pytest.mark.parametrize(
+    ("startsym", "matches"),
+    [("S", 4), ("|", 2), ("-", 2), ("F", 2), ("7", 2), ("L", 2), ("J", 2)],
+)
+def test_valid_neighbor1(grid, startsym, matches):
+    res = [
+        main.is_valid_neighbor(grid(g1, startsym), main.Pos(2, 1), d) for d in main.DIRS
+    ]
+    assert len([r for r in res if r]) == matches
+
+
+g2 = [
+    "..F..",
+    ".LS7.",
+    "..J..",
+]
+
+
+@pytest.mark.parametrize(
+    ("startsym", "matches"),
+    [("S", 4), ("|", 2), ("-", 2), ("F", 2), ("7", 2), ("L", 2), ("J", 2)],
+)
+def test_valid_neighbor2(grid, startsym, matches):
+    res = [
+        main.is_valid_neighbor(grid(g2, startsym), main.Pos(2, 1), d) for d in main.DIRS
+    ]
+    assert len([r for r in res if r]) == matches
+
+
+g3 = [
+    "..L..",
+    ".JSF.",
+    "..7..",
+]
+
+
+@pytest.mark.parametrize(
+    ("startsym", "matches"),
+    [("S", 0), ("|", 0), ("-", 0), ("F", 0), ("7", 0), ("L", 0), ("J", 0)],
+)
+def test_valid_neighbor3(grid, startsym, matches):
+    res = [
+        main.is_valid_neighbor(grid(g3, startsym), main.Pos(2, 1), d) for d in main.DIRS
+    ]
+    assert len([r for r in res if r]) == matches
+
+
+g4 = [
+    "..7..",
+    ".FSJ.",
+    "..L..",
+]
+
+
+@pytest.mark.parametrize(
+    ("startsym", "matches"),
+    [("S", 4), ("|", 2), ("-", 2), ("F", 2), ("7", 2), ("L", 2), ("J", 2)],
+)
+def test_valid_neighbor4(grid, startsym, matches):
+    res = [
+        main.is_valid_neighbor(grid(g4, startsym), main.Pos(2, 1), d) for d in main.DIRS
+    ]
+    assert len([r for r in res if r]) == matches
+
+
+g5 = [
+    "..F..",
+    ".FSF.",
+    "..F..",
+]
+
+
+@pytest.mark.parametrize(
+    ("startsym", "matches"),
+    [("S", 2), ("|", 1), ("-", 1), ("F", 0), ("7", 1), ("L", 1), ("J", 2)],
+)
+def test_valid_neighbor5(grid, startsym, matches):
+    res = [
+        main.is_valid_neighbor(grid(g5, startsym), main.Pos(2, 1), d) for d in main.DIRS
+    ]
+    assert len([r for r in res if r]) == matches
