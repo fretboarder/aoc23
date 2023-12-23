cf057331307acb441feed90eb7bfc09a2e430833
fretboarder <cini01@gmail.com>
2023-12-23 14:20:11 +0100

    refactor: day23 part 1

--- src/aoc23/aoc23/main.py
+++ src/aoc23/aoc23/main.py
@@ -10,56 +10,43 @@ from aoc23.support import get_input
 
 
 class Pos(NamedTuple):
-    col: int  # x
-    row: int  # y
+    x: int  # x
+    y: int  # y
 
 
 Tree = dict[Pos, list[Pos]]
 
+en = enumerate
+
 
 def parse_maze(lines: list[str], honor_slopes: bool) -> Tree:
-    if honor_slopes:
-        grid = [list(row) for row in lines]
-    else:
-        grid = [
-            list(
-                row.replace("<", ".")
-                .replace(">", ".")
-                .replace("v", ".")
-                .replace("^", ".")
-            )
-            for row in lines
-        ]
-
+    grid = [list(row) for row in lines]
     ndict = {"^": (-1, 0), "v": (1, 0), "<": (0, -1), ">": (0, 1)}
     positions = (
-        Pos(col, row)
-        for row, line in enumerate(lines)
-        for col, char in enumerate(line)
-        if char in ".<>^v"
+        Pos(x, y) for y, line in en(lines) for x, char in en(line) if char in ".<>^v"
     )
     tree: Tree = defaultdict(lambda: [])
-    for pos in positions:
-        char = grid[pos.row][pos.col]
-        if char in ndict:
+    for p in positions:
+        char = grid[p.y][p.x]
+        if honor_slopes and char in ndict:
             offs_row, offs_col = ndict[char]
-            tree[pos].append(Pos(pos.col + offs_col, pos.row + offs_row))
+            tree[p].append(Pos(p.x + offs_col, p.y + offs_row))
         else:
             for offs_row, offs_col in ndict.values():
-                neighbor = Pos(pos.col + offs_col, pos.row + offs_row)
+                nb = Pos(p.x + offs_col, p.y + offs_row)
                 if (
-                    0 <= neighbor.row < len(grid)
-                    and 0 <= neighbor.col <= len(grid[0])
-                    and grid[neighbor.row][neighbor.col] != "#"
+                    0 <= nb.y < len(grid)
+                    and 0 <= nb.x <= len(grid[0])
+                    and grid[nb.y][nb.x] != "#"
                 ):
-                    tree[pos].append(neighbor)
+                    tree[p].append(nb)
 
     return tree
 
 
 def dfs_rec(tree: Tree, start: Pos, stop: Pos, visited: set[Pos]) -> int:
-    frontier = deque([(start, 0)])
     steps = 0
+    frontier = deque([(start, steps)])
     while frontier:
         current, steps = frontier.pop()
         if current == stop:
@@ -71,6 +58,7 @@ def dfs_rec(tree: Tree, start: Pos, stop: Pos, visited: set[Pos]) -> int:
         if len(unvis_neighbors) == 1:
             frontier.append((unvis_neighbors[0], steps + 1))
         elif len(unvis_neighbors) > 1:
+            # on a branching point, recursively start a new dfs
             sub_res = [
                 1 + dfs_rec(tree, n, stop, set(visited)) for n in unvis_neighbors
             ]
@@ -81,8 +69,7 @@ def dfs_rec(tree: Tree, start: Pos, stop: Pos, visited: set[Pos]) -> int:
 
 def solution1(tree: Tree, start: Pos, stop: Pos) -> int:
     visited: set[Pos] = set()
-    res = dfs_rec(tree, start, stop, visited)
-    return res
+    return dfs_rec(tree, start, stop, visited)
 
 
 def solution2(tree: Tree, start: Pos, stop: Pos) -> int:
@@ -90,7 +77,7 @@ def solution2(tree: Tree, start: Pos, stop: Pos) -> int:
 
 
 def main() -> tuple[int, int]:
-    lines = get_input(Path(__file__).parent / "ex01.txt")
+    lines = get_input(Path(__file__).parent / "input01.txt")
     tree1, tree2 = (parse_maze(lines, slope) for slope in (True, False))
     start, stop = Pos(lines[0].index("."), 0), Pos(lines[-1].index("."), len(lines) - 1)
     return solution1(tree1, start, stop), solution2(tree2, start, stop)
